57f34907fe052fb365864b342a7adfbc748a1f4c
Neobiano <fabianolisboa@gmail.com>
2025-03-27 22:39:31 -0300

    Adicionando redirecionamento da raiz para a página do mapa

--- propriedades/urls.py
+++ propriedades/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='mapa/', permanent=True), name='index'),
     path('mapa/', views.mapa_view, name='mapa'),
     path('api/propriedades/', views.propriedades_api, name='propriedades_api'),
     path('api/cidades/<str:estado>/', views.cidades_api, name='cidades_api'),
