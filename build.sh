e0a518eb75bb704eec781950672547b4a9ada721
Neobiano <fabianolisboa@gmail.com>
2025-04-06 14:21:52 -0300

    Adiciona criação automática do site no build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Criar superusuário apenas se não existir
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell 
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
+
+# Criar o site se não existir
+echo "from django.contrib.sites.models import Site; Site.objects.get_or_create(id=1, defaults={'domain': 'imoveis-caixa.onrender.com', 'name': 'imoveis-caixa.onrender.com'})" | python manage.py shell 
